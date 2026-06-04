202934d2ea7def3a3de9a49ec22c1d7a3ae5c7da
jeel tank <jeeltank.developer@gmail.com>
2026-06-04 23:20:20 +0530

    creatde model

--- farmer/models.py
+++ farmer/models.py
@@ -34,6 +34,7 @@ class Farmer(models.Model):
     photo = models.ImageField(upload_to='farmer/profile_photos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return f"farmer : {self.user.name}"
