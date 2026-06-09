d19fd83e2d6ecfafc545dbe36518c013ee3ecb2a
jeel tank <jeeltank.developer@gmail.com>
2026-06-09 20:59:05 +0530

    home

--- farmer/views.py
+++ farmer/views.py
@@ -121,13 +121,12 @@ def crop_price(city):
     api_key = "579b464db66ec23bdd0000012bddf55026c442586adb6f1fa0b82807"
     resource_id = "9ef84268-d588-465a-a308-a864a43d0070"
     
-    url = f"https://api.data.gov.in/resource/{resource_id}?api-key={api_key}&format=json&filters[state]=Gujarat&filters[district]={city}&limit=100"
+    url = f"https://api.data.gov.in/resource/{resource_id}?api-key={api_key}&format=json&filters[state]=Gujarat&filters[district]={city}&limit=1000"
     
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     }
     
-    crops = ["Ground Nut Seed", "Wheat", "Bengal Gram", "Green Gram", "Cotton", "Jowar", "Corriander Seed", "Methi Seeds", "Castor Seed", "Red Gram", "Mustard", "Sesamum", "Soyabean", "Black Gram", "Cummin Seed"]
     prices = []
     
     try:
@@ -136,14 +135,12 @@ def crop_price(city):
         records = data.get("records", [])
         
         # Look for our target crops in the records
-        for crop in crops:
-            for record in records:
-                if record.get("commodity") == crop:
-                    prices.append({
-                        "crop": crop,
-                        "price": record.get("modal_price")/100 * 20
-                    })
-                    break  # Stop searching for this crop once we find the first price
+        for record in records:
+                prices.append({
+                    "crop": record.get("commodity"),
+                    "price": record.get("modal_price") / 100 * 20,  # Convert to price per 20kg
+                    "price_range": f"{record.get('min_price')} - {record.get('max_price')}"
+                })  # Stop searching for this crop once we find the first price
                     
     except Exception as e:
         print(f"Error fetching data: {e}")
@@ -153,9 +150,10 @@ def crop_price(city):
 @check_login(['Farmer'])
 def farmer_home(request):
     uid = request.uid
-    city = uid.city if uid.city else "Rajkot"
+    city = uid.city
     crop_prices = crop_price(city)
-    context = {"crop_prices": crop_prices}
+    crop_prices = crop_prices[:9]
+    context = {"crop_prices": crop_prices,'uid' : uid}
 
     return render(request, "farmer/home.html",context)
 
