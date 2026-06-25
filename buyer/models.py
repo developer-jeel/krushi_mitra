8cb318bcc0f4c371037fd606f199122c362b6b7b
jeel tank <jeeltank.developer@gmail.com>
2026-06-25 13:48:42 +0530

    add is_readed field to notifications model and implement notifications list view template

--- buyer/models.py
+++ buyer/models.py
@@ -184,7 +184,9 @@ class notifications(models.Model):
     user = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='buyer')
     notification_type = models.CharField(max_length=20,choices=NOTIFICATIONS_TYPES)
     message = models.CharField(max_length=100,blank=True,null=True)
+    is_readed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.user} : {self.notification_type}"
