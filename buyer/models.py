562961ad1287c2abbc15195dad89f3eb243d4ab1
jeel tank <jeeltank.developer@gmail.com>
2026-06-25 13:36:54 +0530

    add notification model and register in admin for buyer system

--- buyer/models.py
+++ buyer/models.py
@@ -172,3 +172,18 @@ class OrderItem(models.Model):
     subtotal = models.DecimalField(max_digits=10,decimal_places=2)
     def __str__(self):
         return self.crop_name
+
+class notifications(models.Model):
+    NOTIFICATIONS_TYPES= ( 
+        ("Order","Order"),
+        ("Payment","Payment"),
+        ("Bulk","Bulk"),
+        ("Export","Export"),
+                            )
+    user = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='buyer')
+    notification_type = models.CharField(max_length=20,choices=NOTIFICATIONS_TYPES)
+    message = models.CharField(max_length=100,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} : {self.notification_type}"
